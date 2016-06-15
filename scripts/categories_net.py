4b10bcd1215da4174bfb798cb8d3b26f8e48e217
wattik <najmanm@gmail.com>
2016-06-15 12:37:06 +0200

    CategoriesNetHelper() created.

---
+++ scripts/categories_net.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+__author__ = 'Wattik'
+
+class CategoriesNetHelper():
+
+    _order = 2
+
+    def __init__(self, order):
+        self._order = order
+
+    def get_topics(self):
+        return dict()
+
