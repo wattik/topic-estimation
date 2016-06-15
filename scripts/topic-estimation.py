bf18a1b93729526bc16fc1ceb241000ac15a17e1
wattik <najmanm@gmail.com>
2016-06-15 12:36:29 +0200

    The file set up to use CategoriesNetHelper().

--- scripts/topic-estimation.py
+++ scripts/topic-estimation.py
@@ -6,6 +6,7 @@ __author__ = 'Wattik'
 import sys
 from nltk.tokenize import word_tokenize
 from nltk.util import ngrams
+from categories_net import CategoriesNetHelper
 
 """
 The script estimates topics of a short text based on Wikipedia categories.
@@ -22,77 +23,83 @@ The output is a set of topics. (TBD...)
 
 # Functions ##################################
 
-def estimate_topics(text):
+class TopicEstimator():
 
-    if len(text) == 0:
-        raise Exception("Text is not long enough.")
+    @staticmethod
+    def estimate(self, text):
 
-    # Basic preprocessing such as: utf-8 encoding, lowercasing
-    text = preprocess(text)
+        if len(text) == 0:
+            raise Exception("Text is not long enough.")
 
-    # Breaking apart into n-grams, so far n=3
-    tokens = tokenize(text, 3)
+        # Basic preprocessing such as: utf-8 encoding, lowercasing
+        text = self._preprocess(text)
 
-    # TODO: ?????
-    # additional layers and filters here
-    # Synonyms etc
-    # Stemmatization, lemmatization, stopwords, etc.
+        # Breaking apart into n-grams, so far n=3
+        tokens = self._tokenize(text, 3)
 
-    # Given tokens find categories in wiki's net. Go 2 levels deep.
-    proposed_topics = find_topics(tokens, 2)
+        # TODO: ?????
+        # additional layers and filters here
+        # Synonyms etc
+        # Stemmatization, lemmatization, stopwords, etc.
 
-    # Choose the best proposals of all proposed topics.
-    topics = filter_topics(proposed_topics)
+        # Given tokens find categories in wiki's net. Go 2 levels deep.
+        proposed_topics = self._find_topics(tokens, 2)
 
-    return topics
+        # Choose the best proposals of all proposed topics.
+        topics = self._filter_topics(proposed_topics)
 
-
-def preprocess(text):
-    text = unicode(text, 'utf-8')
-    text = text.lower()
-    return text
+        return topics
 
 
-def tokenize(text, n):
-    if n < 1:
-        raise Exception("N-grams require n >= 1.")
-
-    # Create tokens from the text.
-    unigrams = word_tokenize(text)
-
-    # Get rid of '.', ',' and etc.
-    blacklist = [u'.', u',']
-    unigrams = [unigram for unigram in unigrams if unigram not in blacklist]
-
-    # Create n-grams
-    tokens = unigrams
-    for i in xrange(2, n+1):
-        tokens = tokens + list(ngrams(unigrams, i))
-
-    return tokens
+    def _preprocess(self, text):
+        text = unicode(text, 'utf-8')
+        text = text.lower()
+        return text
 
 
-def find_topics(tokens, order):
-    topics = dict()
-    for token in tokens:
+    def _tokenize(self, text, n):
+        if n < 1:
+            raise Exception("N-grams require n >= 1.")
+
+        # Create tokens from the text.
+        unigrams = word_tokenize(text)
+
+        # Get rid of '.', ',' and etc.
+        blacklist = [u'.', u',']
+        unigrams = [unigram for unigram in unigrams if unigram not in blacklist]
+
+        # Create n-grams
+        tokens = unigrams
+        for i in xrange(2, n+1):
+            tokens = tokens + list(ngrams(unigrams, i))
+
+        return tokens
 
 
-    proposed_topics = tokens
-    return proposed_topics
+    def _find_topics(self, tokens, order):
+
+        helper = CategoriesNetHelper(order)
+
+        topics = dict()
+        for token in tokens:
+            topics = topics + helper.get_topics(token)
+
+        proposed_topics = tokens
+        return proposed_topics
 
 
-def filter_topics(proposed_topics):
-    # TODO
-    topics = proposed_topics
-    return topics
+    def _filter_topics(self, proposed_topics):
+        # TODO
+        topics = proposed_topics
+        return topics
 
 
 # Main: ##################################
 
 if __name__ == "__main__":
-    #topics = estimate_topics(sys.argv[1])
 
-    # TODO: Return the topic. Could be just a set? Maybe dictionary of key and confidence.
+    # TODO: change here after beta is done.
 
-
-    print estimate_topics("The world isn't fair")
+    text = "Shoot for the start 'cause if you miss, you might end up on the moon."
+    # text = TopicEstimator.estimate(sys.argv[1])
+    print TopicEstimator.estimate(text)
