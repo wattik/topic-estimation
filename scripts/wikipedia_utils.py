9859cc052bb10657e0a48af520d1582228efc3e2
wattik <najmanm@gmail.com>
2016-07-08 16:05:22 +0200

    Support of disambiguation pages

--- scripts/wikipedia_utils.py
+++ scripts/wikipedia_utils.py
@@ -23,7 +23,7 @@ class AbstractWikipedia(object):
     def _is_disambiguation_page(self, name):
 
         __DISAMBIGUATION_CATEGORIES = {'cs': u'Wikipedie:Rozcestníky',
-                                       'en': u'Disambiguation_pages'} #TODO check
+                                       'en': u'Disambiguation_pages'} #TODO check for EN
 
         if type(name) == unicode or type(name) == str:
             if name == __DISAMBIGUATION_CATEGORIES[self.lang]:
@@ -36,6 +36,12 @@ class AbstractWikipedia(object):
 
         return False
 
+    def _unicode(self, items):
+        temp = []
+        for item in items:
+            if item is not unicode: item = unicode(item, "utf-8")
+            temp.append(item)
+        return temp
 
 class WikipediaBrowser(AbstractWikipedia):
     """
@@ -196,41 +202,64 @@ class WikipediaMySQL(AbstractWikipedia):
 
 class WikipediaRedis(AbstractWikipedia):
 
-    def __init__(self, lang = "en"):
-        if lang != "en":
+    def __init__(self, lang = "cs"):
+        # just up until multiple langs are supported
+        if lang != "cs":
             self.change_language(lang)
+
         AbstractWikipedia.__init__(self,lang)
         self.r = redis.StrictRedis()
 
-        self.prep = {"page":"p:", "category": "c:", "redirect":"r:"}
-
+        self.prep = {"page":"p:", "category":"c:", "redirect":"r:", "pagelinks":"l:"}
 
     def get_page_categories(self, name):
+        # Initialize.
         categories = []
         gen_by = "redis"
 
-        # Check if there is any page of this name
-        page_id = self.r.get(self.prep["page"] + name)
-        if page_id is None:
+        # Check if there is any page of this name.
+        list_of_page_proposals = self.r.lrange(self.prep["page"] + name, 0, -1)
+
+        # In case of an empty list = page not found.
+        if not list_of_page_proposals:
             return categories, gen_by
 
-        # if it leads to redirect, get the redirect (assuming only one redirect)
+        # The list is not empty, we've got proposals.
+        for page_id in list_of_page_proposals:
+            categories = categories + self._single_page_id_to_categories(page_id)
+
+        return categories, gen_by
+
+    def _single_page_id_to_categories(self, page_id):
+        # If it leads to a redirect, get the redirect's page id.
         redirect_name = self.r.get(self.prep["redirect"] + page_id)
         if redirect_name is not None:
             page_id = self.r.get(self.prep["page"] + redirect_name)
 
-        # get its categories
-        response = self.r.lrange(self.prep["category"] + page_id, 0, -1)
+        # Get the page's categories
+        proposed_categories = self.r.lrange(self.prep["category"] + page_id, 0, -1)
 
-        # convert them into unicode and
-        for i in response:
-            if i is not unicode: i = unicode(i, "utf-8")
-            # TODO: if is disambigous, find its links.
-            #       now only passes
-            categories.append(i)
+        # convert string items (categories) into unicode
+        proposed_categories = self._unicode(proposed_categories)
 
-        return categories, gen_by
+        # Check whether this page is not a disambiguatino page.
+        if self._is_disambiguation_page(proposed_categories):
+            # If so, get its links
+            links = self.r.lrange(self.prep["pagelinks"] + page_id, 0, -1)
+            links = self._unicode(links)
+            links = self._filter_links(links)
+            proposed_categories = links
 
+        return proposed_categories
+
+
+    def _filter_links(self, links):
+        temp = []
+        black_list = [u'název_článku', u'odkaz_na_rozcestník', u'rozcestník', u'článek']
+        for link in links:
+            if link not in black_list:
+                temp.append(link)
+        return temp
 
     def change_language(self, lang):
-        raise NotImplementedError("When using REDIS, only the ENGLISH language is available.")
+        raise NotImplementedError("When using REDIS, so far only INITIAL (depending on redis instance's data) language is available.")
